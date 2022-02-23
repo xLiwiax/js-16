6ad9011bc479e9211596d3c390ea9e677c14f63b
xLiwiax <boliwia333@gmail.com>
2022-02-23 10:41:03 +0100

    zadanie 16

---
+++ main.js
@@ -0,0 +1,10 @@
+function fun(a, b) {
+    suma = a + b;
+    roznica = a - b;
+    mnozenie = a * b;
+    console.log("Wynik dodawania wynosi ", suma);
+    console.log("Wynik odejmowania wynosi ", roznica);
+    console.log("Wynik mnożenia wynos ", mnozenie);
+}
+
+fun(8, 3)
